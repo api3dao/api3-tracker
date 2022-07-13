bb83c99e9feb64adddcc8fe60c427b705eb9392b
whitewingshorse <messages@webcerebrium.com>
2022-07-13 11:49:20 +0300

    stories: nextjs providers

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "storybook-addon-next",
+    "storybook-addon-next-router"
   ],
   "framework": "@storybook/react",
   "core": {
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,10 @@
+import { RouterContext } from "next/dist/shared/lib/router-context"; // next 12
+import { withNextRouter } from 'storybook-addon-next-router';
+
+export const decorators = [
+  withNextRouter
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -5,5 +12,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider,
   },
 }
