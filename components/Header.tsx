a36f53b562d3b7fc4b88ba1279b21addfb9959a4
whitewingshorse <messages@webcerebrium.com>
2022-07-14 10:06:22 +0300

    tailwindcss for the app

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Menu } from "./Menu";
 
 interface IHeader {
-  active: string
+  active: string;
 }
 
 export const Header = (props: IHeader) => {
@@ -10,14 +10,17 @@ export const Header = (props: IHeader) => {
     <header className="">
       <div className="inner">
         <div className="nav-brand">
-          <span className="nav-brand__label">API3 DAO Tracker</span>
+          <span className="nav-brand__label">
+            <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+            API3 DAO Tracker
+          </span>
           <span className="nav-brand__slogan">
             <span></span>on-chain analytics: members, staking rewards, API3
             token supply
           </span>
         </div>
         <div className="mid"></div>
-        <Menu active={props.active}  />
+        <Menu active={props.active} />
       </div>
     </header>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./stories/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkmode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
