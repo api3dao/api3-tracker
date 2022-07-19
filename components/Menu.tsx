7be268b38f9a22fbc32a3878f06237a7578ceb0f
whitewingshorse <messages@webcerebrium.com>
2022-07-19 13:01:18 +0300

    fmt

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -38,15 +38,21 @@ export const Menu = (props: IMenuProps) => {
   return (
     <div className="lg:flex lg:items-end">
       {menuItems.map((item) => {
-        const classHover = "hover:text-color-menu-hover hover:border-color-menu-hover";
-        const classCommon = "block text-center leading-10 lg:inline lg:mx-1 lg:px-2";
+        const classHover =
+          "hover:text-color-menu-hover hover:border-color-menu-hover";
+        const classCommon =
+          "block text-center leading-10 lg:inline lg:mx-1 lg:px-2";
         const classFont = "text-bold lg:border-b-4 lg:border-b-solid";
 
-        const classActive = "bg-color-menu-active text-color-body lg:bg-transparent lg:text-color-menu-active lg:border-color-menu-active";
-        const classInactive ="text-color-grey lg:border-transparent";
-        const classFull = [classHover, classCommon, classFont, (
-           props.active == item.href ? classActive : classInactive
-        )].join(" ");
+        const classActive =
+          "bg-color-menu-active text-color-body lg:bg-transparent lg:text-color-menu-active lg:border-color-menu-active";
+        const classInactive = "text-color-grey lg:border-transparent";
+        const classFull = [
+          classHover,
+          classCommon,
+          classFont,
+          props.active == item.href ? classActive : classInactive,
+        ].join(" ");
 
         return (
           <Link href={item.href} key={item.href}>
