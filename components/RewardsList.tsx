f3eddf6f64de15c64f710fd4d07b480bac29a3f4
EnormousCloud <enormous@webcerebrium.com>
2022-07-22 11:26:54 +0300

    rewards: alignments and containers fixes

--- components/RewardsList.tsx
+++ components/RewardsList.tsx
@@ -8,8 +8,8 @@ export const RewardsListThead = () => (
       <th className="text-center">Epoch</th>
       <th className="text-center">Block</th>
       <th className="text-center">Rewards Date</th>
-      <th className="text-right">APR</th>
-      <th className="text-right">Rewards</th>
+      <th className="text-center">APR</th>
+      <th className="text-center">Rewards</th>
       <th className="text-right">Members</th>
       <th className="text-right">Staked</th>
       <th className="text-right">Minted</th>
@@ -31,8 +31,8 @@ export const RewardsListTr = (epoch: IEpoch) => (
       </a>
     </td>
     <td className="text-center">{niceDate(epoch.createdAt)}</td>
-    <td className="text-right darken">{epoch.apr}%</td>
-    <td className="text-center accent">{epoch.rewardsPct}%</td>
+    <td className="text-center darken">{epoch.apr+'%'}</td>
+    <td className="text-center accent">{epoch.rewardsPct+'%'}</td>
     <td className="text-right darken">{toCurrency(epoch.members)}</td>
     <td className="text-right darken">{toCurrency(epoch.totalStake)}</td>
     <td className="text-right accent">{toCurrency(epoch.mintedShares)}</td>
@@ -46,7 +46,7 @@ export interface IRewardsListProps {
 
 export const RewardsList = (props: IRewardsListProps) => {
   return (
-    <div>
+    <div className="max-w-screen-lg mx-auto">
       <table className="table invisible lg:visible">
         <RewardsListThead />
         <tbody>
--- stories/RewardsList.stories.tsx
+++ stories/RewardsList.stories.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Prisma } from "@prisma/client";
 import { RewardsList } from "../components/RewardsList";
 
 export default {
@@ -18,17 +19,17 @@ export const Default = Template.bind({});
 Default.args = {
   list: [
     {
-      isCurrent: true,
+      isCurrent: 1,
       epoch: 3333,
-      createdAt: new Date(),
-      apr: 14.75,
-      rewardsPct: 0.2829,
+      createdAt: new Date().toISOString(),
+      apr: new Prisma.Decimal(14.75),
+      rewardsPct: new Prisma.Decimal(0.2829),
       members: 8000,
       totalStake: 55481434,
       stakedRewards: 13756546,
       totalShares: 58903799,
       mintedShares: 156944,
-      releaseDate: new Date(),
+      releaseDate: new Date().toISOString(),
       blockNumber: 2828258,
       blockTx:
         "0xd29d9e55df72365e1e9b096b8850a6b0f612819d47d1175055c380721dcc8d18",
