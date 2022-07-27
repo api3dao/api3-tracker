31a7ff8c0d98938cff95d7908f33fc015e98275f
whitewingshorse <messages@webcerebrium.com>
2022-07-27 14:39:45 +0300

    fetch: treasuries

--- pages/treasury.tsx
+++ pages/treasury.tsx
@@ -3,19 +3,27 @@ import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { fetchWebconfig } from "../services/webconfig";
 import { Meta } from "../components/Meta";
+import { ITreasuryType, Treasuries } from "../services/treasuries";
+import superjson from "superjson";
 
 export async function getServerSideProps() {
   const webconfig = fetchWebconfig();
+  const names = await Treasuries.fetchList();
+  const list = await Promise.all(
+    names.map(async (ttype: ITreasuryType) => {
+      return await Treasuries.fetch(ttype);
+    })
+  );
   return {
     props: {
       webconfig,
+      list: JSON.parse(superjson.stringify(list)).json,
     }, // will be passed to the page component as props
   };
 }
 
 const TreasuryPage: NextPage = (props: any) => {
-  // TODO: split into components
-  const { webconfig } = props;
+  const { list, webconfig } = props;
 
   return (
     <div>
@@ -29,6 +37,7 @@ const TreasuryPage: NextPage = (props: any) => {
             API3 DAO currently operates 3 treasuries. Balances below are updated
             each hour.
           </p>
+          <pre>{JSON.stringify(list, null, 2)}</pre>
         </div>
       </main>
 
---
+++ services/treasuries.ts
@@ -0,0 +1,25 @@
+import { prisma } from "./db";
+import { TreasuryType } from ".prisma/client";
+import { ITreasury } from "./../services/api3";
+
+export type ITreasuryType = TreasuryType;
+
+export const Treasuries = {
+  // fetch a list of treasuries
+  fetchList: async (): Promise<Array<TreasuryType>> => {
+    return (
+      await prisma.treasury.groupBy({
+        by: ["ttype"],
+        orderBy: { ttype: "asc" },
+      })
+    ).map((x: any) => x.ttype as TreasuryType);
+  },
+  // fetch the latest values of the given treasury
+  fetch: async (ttype: TreasuryType): Promise<Array<any>> => {
+    return (await prisma.treasury.findMany({
+      where: { ttype, current: 1 },
+      orderBy: { ts: "desc" },
+      take: 1,
+    })).map((x: any) => ({ ...x }));
+  },
+};
