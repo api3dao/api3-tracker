3ae3e5f9d04041145f87a59d2518eb41ea3117e2
EnormousCloud <enormous@webcerebrium.com>
2022-08-01 16:58:18 +0300

    fetcher: wallet and events

--- services/votings.ts
+++ services/votings.ts
@@ -2,7 +2,6 @@ import prisma from "./db";
 import { Prisma } from "@prisma/client";
 import { VotingType } from ".prisma/client";
 import { IVoting, IVotingEvent } from "./api3";
-import { toHex } from "./format";
 
 const Decimal = Prisma.Decimal;
 export type IVotingType = VotingType;
