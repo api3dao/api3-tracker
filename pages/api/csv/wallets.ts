b95c2084a8ce4c5ed10727ce888817a80ae6edb0
whatifitrains <victorathome@gmail.com>
2022-08-18 12:54:14 +0300

    wallets csv: use list of provided columns

--- pages/api/csv/wallets.ts
+++ pages/api/csv/wallets.ts
@@ -19,35 +19,47 @@ const NAMES = [
   "UPDATED",
 ];
 
-const INDEXES = {
-  ADDRESS: 0,
-  ENS: 1,
-  BADGES: 2,
-};
-
 const toArray = (src: IWallet) => [
   toHex(src.address),
   src.ensName,
   src.badges,
-  src.userShare + '',
-  src.userStake + '',
-  src.userVotingPower + '',
-  src.userReward + '',
-  src.userLockedReward + '',
-  src.userDeposited + '',
-  src.userWithdrew + '',
+  src.userShare + "",
+  src.userStake + "",
+  src.userVotingPower + "",
+  src.userReward + "",
+  src.userLockedReward + "",
+  src.userDeposited + "",
+  src.userWithdrew + "",
   niceDate(src.createdAt),
   niceDate(src.updatedAt),
 ];
 
+const rearrange = (columns: Array<number>, full: Array<string>): Array<string> => {
+   if (columns.length === 0) return full;
+   return columns.map((ci: number) => (full[ci]));
+};
+
 export default async function handler(
-  _req: NextApiRequest,
+  req: NextApiRequest,
   res: NextApiResponse<string>
 ) {
-  const out = [NAMES];
+  const columns = [];
+  if (req.query.columns) {
+    let cols = (req.query.columns as string).split(",");
+    for (let ci = 0; ci < cols.length; ci++) {
+      let found = NAMES.indexOf(cols[ci].toUpperCase());
+      if (found === -1) {
+        res.status(400).send("ERROR: invalid column " + cols[ci]);
+        return;
+      }
+      columns.push(found);
+    }
+  }
+
+  const out = [rearrange(columns, NAMES)];
   const list: Array<IWallet> = await Wallets.fetchList();
   for (let index = 0; index < list.length; index++) {
-    out.push(toArray(list[index]));
+    out.push(rearrange(columns, toArray(list[index])))
   }
   res.status(200).send(stringify(out));
 }
