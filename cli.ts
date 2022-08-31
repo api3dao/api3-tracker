3e6d1da4561191d103d9bf9529af3743628e04bc
whatifitrains <victorathome@gmail.com>
2022-08-31 19:23:37 +0300

    cli: json+rpc endpoint config

--- cli.ts
+++ cli.ts
@@ -2,17 +2,27 @@ import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 
 yargs(hideBin(process.argv))
+  .env("API3TRACKER")
+  .option("endpoint", {
+    default: "http://localhost:8545/",
+    alias: "e",
+    type: "string",
+    description: "Ethereum JSON+RPC endpoint",
+  })
   .command({
     command: "logs [sub]",
     describe: "Operations with API3 DAO contract logs",
     builder: (yargs) => {
       return yargs.option(`sub`, {
         choises: ["reset", "download"],
-        type: `string`,
+        type: "string",
         describe: `logs subcommand - reset or download new`,
       });
     },
-    handler: ({ sub }) => {
+    handler: ({ endpoint, sub }) => {
+      console.log("API3TRACKER_ENDPOINT", process.env.API3TRACKER_ENDPOINT);
+      console.log("endpoint: ", endpoint);
+
       if (sub == "reset") {
         console.log("resetting");
       } else if (sub == "download") {
