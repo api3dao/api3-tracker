c526fa8682bc53d31953f73677b59eb083edd96a
whatifitrains <victorathome@gmail.com>
2022-09-07 09:38:29 +0300

    ethereum price client: with daily cache

--- services/price.ts
+++ services/price.ts
@@ -1,11 +1,18 @@
 import axios from "axios";
 
 export const EthereumPrice = {
+  cached: new Map<string, number>(),
   at: async (dt: Date): Promise<number> => {
     const isoDt = dt.toISOString().substring(0, 10);
+    const fromCache = EthereumPrice.cached.get(isoDt);
+    if (fromCache) {
+      return fromCache;
+    }
     const response = await axios.get(
       `https://enormous.cloud/prices/api/ethereum/at/${isoDt}`
     );
-    return (response as any).data.markets.ethereum.usd;
+    const out: number = (response as any).data.markets.ethereum.usd;
+    EthereumPrice.cached.set(isoDt, out);
+    return out;
   },
 };
