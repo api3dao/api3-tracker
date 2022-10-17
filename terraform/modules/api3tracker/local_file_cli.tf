ffdd55493d389aaa77bc76a0a1ad369528fb3c84
whatifitrains <victorathome@gmail.com>
2022-10-17 11:12:36 +0300

    Crontab fix

--- terraform/modules/api3tracker/local_file_cli.tf
+++ terraform/modules/api3tracker/local_file_cli.tf
@@ -3,7 +3,7 @@ resource "local_file" "cli" {
 #!/usr/bin/env bash
 set -x
 docker exec -e TS_NODE_PROJECT=./tsconfig.cli.json \
-   -it ${local.hostname} yarn ts-node cli.ts $@
+   -i ${local.hostname} yarn ts-node cli.ts $@
 EOF
 
    filename = "./bin/cli.sh"
--- terraform/modules/api3tracker/local_file_crontab.tf
+++ terraform/modules/api3tracker/local_file_crontab.tf
@@ -1,9 +1,9 @@
 resource "local_file" "crontab_api3tracker" {
   content = <<EOF
-*/10 * * * * root cd ${path.cwd} && ./bin/cli.sh logs download 2>&1 >> /var/logs/api3-logs-download.log
-15,45 * * * * root cd ${path.cwd} && ./bin/cli.sh supply download >> /var/logs/api3-supply-download.log
-0 * * * * root cd ${path.cwd} && ./bin/cli.sh treasuries download >> /var/logs/api3-treasuries-download.log
-10 * * * * root cd ${path.cwd} && ./bin/cli.sh state update >> /var/logs/api3-state-update.log
+*/10 * * * * root cd ${path.cwd} && ./bin/cli.sh logs download 2&>1 >> /var/log/api3-logs-download.log
+15,45 * * * * root cd ${path.cwd} && ./bin/cli.sh supply download 2&>1 >> /var/log/api3-supply-download.log
+0 * * * * root cd ${path.cwd} && ./bin/cli.sh treasuries download 2&>1 >> /var/log/api3-treasuries-download.log
+# 10 * * * * root cd ${path.cwd} && ./bin/cli.sh state update 2&>1 >> /var/log/api3-state-update.log
 EOF
 
   filename = "./cron.d/crontab_api3tracker"
