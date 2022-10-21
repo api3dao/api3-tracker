6726369aeb94e1f2fafbfeabca224d3ec469d437
EnormousCloud <enormous@webcerebrium.com>
2022-10-21 19:26:53 +0300

    backups: put on daily crontab

---
+++ terraform/modules/postgres/lcoal_file_crontab_postgres.tf
@@ -0,0 +1,9 @@
+resource "local_file" "crontab_postgres" {
+  content = <<EOF
+0 * * * * root cd ${path.cwd} && bash ./bin/postgres-backup.sh >> /var/log/postgres-backups.log 2>&1
+EOF
+
+  filename = "./cron.d/crontab_postgres"
+  file_permission = "0777"
+ }
+
