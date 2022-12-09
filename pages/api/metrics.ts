8a6e692a6b514b022c047a34507129bd9ce14661
EnormousCloud <enormous@webcerebrium.com>
2022-12-09 16:13:18 +0200

    metrics: treasuries

--- pages/api/metrics.ts
+++ pages/api/metrics.ts
@@ -1,5 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ethers } from "ethers";
+import { ITreasuryType, Treasuries, Blocks } from "../../services/api";
+import { toHex, serializable } from "../../services/format";
+import { ITreasury, IBlockNumber } from "../../services/types";
 
 // This function is used for comparison of the chain state sync with cloudflare
 const getHeadBlock = async (): Promise<BigInt> => {
@@ -21,7 +24,9 @@ export default async function handler(
 
   // 1. Block numbers: external, max downloaded block, max porcessed block
   const headBlock = await getHeadBlock();
-  out.push("# HELP head_block_number: latest block number at cloudflare or other external JSON+RPC provider");
+  out.push(
+    "# HELP head_block_number: latest block number at cloudflare or other external JSON+RPC provider"
+  );
   out.push("# TYPE head_block_number gauge");
   out.push("head_block_number " + headBlock);
 
@@ -29,7 +34,9 @@ export default async function handler(
     where: { id: 1 },
   });
   if (status.length > 0) {
-    out.push("# HELP downloaded_block_number: latest downloaded into caches block number ");
+    out.push(
+      "# HELP downloaded_block_number: latest downloaded into caches block number "
+    );
     out.push("# TYPE downloaded_block_number gauge");
     out.push("downloaded_block_number " + status[0].downloaded);
 
@@ -38,7 +45,27 @@ export default async function handler(
     out.push("processed_block_number  " + status[0].processed);
   }
 
-  // 2. TODO: treasuries balances
+  // 2. treasuries balances
+  const names = await Treasuries.fetchList();
+  if (names.length > 0) out.push("");
+  for (const ttype of names) {
+    const tokens = await Treasuries.fetch(ttype);
+    const valueAPI3 =
+      tokens
+        .filter((x: any) => x.token === "API3")
+        .map((x: any) => x.value)[0] || 0;
+    const valueUSDC =
+      tokens
+        .filter((x: any) => x.token === "USDC")
+        .map((x: any) => x.value)[0] || 0;
+    out.push("# HELP treasury_api3: API3 tokens in the treasury");
+    out.push("# TYPE treasury_api3: gauge");
+    out.push('treasury_api3{name="' + ttype + '"} ' + valueAPI3);
+    out.push("# HELP treasury_usdc: USDC tokens in the treasury");
+    out.push("# TYPE treasury_usdc: gauge");
+    out.push('treasury_usdc{name="' + ttype + '"} ' + valueUSDC);
+  }
+
   // 3. TODO: supply
   // 4. TODO: Number of members (per tag)
   // 5. TODO: Number of votes (per status)
