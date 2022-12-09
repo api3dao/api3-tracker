6b015a4d88bd7ade2cb97d777bcaba4b2a513583
EnormousCloud <enormous@webcerebrium.com>
2022-12-09 15:58:57 +0200

    metrics: block numbers

--- pages/api/metrics.ts
+++ pages/api/metrics.ts
@@ -1,17 +1,48 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { ethers } from "ethers";
 
-export default function handler(
+// This function is used for comparison of the chain state sync with cloudflare
+const getHeadBlock = async (): Promise<BigInt> => {
+  const jsonRpc = new ethers.providers.JsonRpcProvider(
+    process.env.API3TRACKER_EXTERNAL_ENDPOINT || "https://cloudflare-eth.com/"
+  );
+  const result = await ethers.utils.fetchJson(
+    jsonRpc.connection,
+    ` {"id":"head","method":"eth_blockNumber","params":[],"jsonrpc":"2.0"} `
+  );
+  return BigInt(result.result);
+};
+
+export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse<string>
 ) {
-  // TODO: treasury balances
-  // TODO: DAO supply
-  // TODO: APR and pool totals
+  const out = ["# HELP up Server is running", "# TYPE up gauge", "up 1", ""];
 
-  res
-    .status(200)
-    .send(
-      ["# HELP up Server is running", "# TYPE up gauge", "up 1"].join("\n")
-    );
+  // 1. Block numbers: external, max downloaded block, max porcessed block
+  const headBlock = await getHeadBlock();
+  out.push("# HELP head_block_number: latest block number at cloudflare or other external JSON+RPC provider");
+  out.push("# TYPE head_block_number gauge");
+  out.push("head_block_number " + headBlock);
+
+  const status = await prisma.syncStatus.findMany({
+    where: { id: 1 },
+  });
+  if (status.length > 0) {
+    out.push("# HELP downloaded_block_number: latest downloaded into caches block number ");
+    out.push("# TYPE downloaded_block_number gauge");
+    out.push("downloaded_block_number " + status[0].downloaded);
+
+    out.push("# HELP processed_block_number: latest processed block number");
+    out.push("# TYPE processed_block_number gauge");
+    out.push("processed_block_number  " + status[0].processed);
+  }
+
+  // 2. TODO: treasuries balances
+  // 3. TODO: supply
+  // 4. TODO: Number of members (per tag)
+  // 5. TODO: Number of votes (per status)
+  // 6. TODO: Shares estimates
+
+  res.status(200).send(out.join("\n"));
 }
