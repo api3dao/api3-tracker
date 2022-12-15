22e11fe0d2cf546d6f404ea2f3965b8d3be91290
EnormousCloud <enormous@webcerebrium.com>
2022-12-15 20:37:58 +0200

    extending snapshots: history of stakes

--- services/shares.ts
+++ services/shares.ts
@@ -47,54 +47,101 @@ const uniqueEvents = async (member: string): Promise<Array<any>> => {
   return out;
 };
 
-const downloadBlock = async (
-  endpoint: string,
-  webconfig: IWebConfig,
-  member: string,
-  blockNumber: number
-) => {
-  const pool: string = (
-    webconfig.contracts?.find(
-      ({ name }) => name.toLowerCase() === "api3pool"
-    ) || { address: "" }
-  ).address;
-
-  const jsonRpc = new ethers.providers.JsonRpcProvider(endpoint);
-  const poolContract = new ethers.Contract(pool, PoolABI, jsonRpc);
-  const userAddress = member;
-  const found = await prisma.cacheUserShares.findMany({
-    where: { addr: Address.asBuffer(userAddress), height: blockNumber },
-  });
-  if (found.length == 0) {
-    console.log("Requesting Block", blockNumber, " for ", member);
-    const shares = withDecimals(
-      (await poolContract.userSharesAt(userAddress, blockNumber)).toString(),
-      18
-    );
-    const votingPower = withDecimals(
-      (
-        await poolContract.userVotingPowerAt(userAddress, blockNumber)
-      ).toString(),
-      18
-    );
-    await prisma.cacheUserShares.create({
-      data: {
-        addr: Address.asBuffer(userAddress),
-        height: blockNumber,
-        shares: new Prisma.Decimal(shares),
-        votingPower: new Prisma.Decimal(votingPower),
-      },
-    });
-    return { shares, votingPower };
-  }
-  return { shares: found[0].shares, votingPower: found[0].votingPower };
-};
-/*
- */
-
 export const Shares = {
   resetAll: async () => {
     await prisma.cacheUserShares.deleteMany({});
+  },
+  downloadTotalsAt: async (
+    endpoint: string,
+    webconfig: IWebConfig,
+    blockNumber: number
+  ) => {
+    const pool: string = (
+      webconfig.contracts?.find(
+        ({ name }) => name.toLowerCase() === "api3pool"
+      ) || { address: "" }
+    ).address;
+
+    const jsonRpc = new ethers.providers.JsonRpcProvider(endpoint);
+    const poolContract = new ethers.Contract(pool, PoolABI, jsonRpc);
+    const found = await prisma.cacheTotalShares.findMany({
+      where: { height: blockNumber },
+    });
+    if (found.length == 0) {
+      console.log("Requesting Block", blockNumber, " totals");
+      const totalStake = withDecimals(
+        (await poolContract.totalStake(blockNumber)).toString(),
+        18
+      );
+      const totalShares = withDecimals(
+        (await poolContract.totalShares(blockNumber)).toString(),
+        18
+      );
+      await prisma.cacheTotalShares.create({
+        data: {
+          height: blockNumber,
+          totalShares: new Prisma.Decimal(totalShares),
+          totalStake: new Prisma.Decimal(totalStake),
+        },
+      });
+      return { totalShares, totalStake };
+    }
+    return found[0];
+  },
+
+  downloadUserAt: async (
+    endpoint: string,
+    webconfig: IWebConfig,
+    member: string,
+    blockNumber: number
+  ) => {
+    const pool: string = (
+      webconfig.contracts?.find(
+        ({ name }) => name.toLowerCase() === "api3pool"
+      ) || { address: "" }
+    ).address;
+
+    const jsonRpc = new ethers.providers.JsonRpcProvider(endpoint);
+    const poolContract = new ethers.Contract(pool, PoolABI, jsonRpc);
+    const userAddress = member;
+    const found = await prisma.cacheUserShares.findMany({
+      where: { addr: Address.asBuffer(userAddress), height: blockNumber },
+    });
+    if (found.length == 0) {
+      console.log("Requesting Block", blockNumber, " for ", member);
+      const shares = withDecimals(
+        (await poolContract.userSharesAt(userAddress, blockNumber)).toString(),
+        18
+      );
+      const votingPower = withDecimals(
+        (
+          await poolContract.userVotingPowerAt(userAddress, blockNumber)
+        ).toString(),
+        18
+      );
+      const stake = withDecimals(
+        (await poolContract.userStake(userAddress, blockNumber)).toString(),
+        18
+      );
+      const locked = withDecimals(
+        (await poolContract.userLocked(userAddress, blockNumber)).toString(),
+        18
+      );
+      const user = await poolContract.users(userAddress, blockNumber);
+      await prisma.cacheUserShares.create({
+        data: {
+          addr: Address.asBuffer(userAddress),
+          height: blockNumber,
+          shares: new Prisma.Decimal(shares),
+          stake: new Prisma.Decimal(stake),
+          locked: new Prisma.Decimal(locked),
+          votingPower: new Prisma.Decimal(votingPower),
+          user,
+        },
+      });
+      return { shares, stake, locked, votingPower, user };
+    }
+    return found[0];
   },
   download: async (endpoint: string, member: string) => {
     const webconfig = fetchWebconfig();
@@ -103,13 +150,24 @@ export const Shares = {
     const events = await uniqueEvents(member);
     console.log("Found ", events.length, "events");
     for (const e of events) {
-      const args = await downloadBlock(
+      const argsUser = await Shares.downloadUserAt(
         endpoint,
         webconfig,
         member,
         e.blockNumber
       );
-      console.log("Block ", e.blockNumber, args);
+      const totals = await Shares.downloadTotalsAt(
+        endpoint,
+        webconfig,
+        e.blockNumber
+      );
+      console.log(
+        "Block ",
+        e.blockNumber,
+        JSON.stringify(argsUser),
+        JSON.stringify(totals)
+      );
+      const args = { ...argsUser, ...totals };
       await prisma.memberEvent.create({
         data: {
           id: e.id + "-s",
