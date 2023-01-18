d61cd3b4b4ad30d98216ef78b4a61415683e838e
EnormousCloud <enormous@webcerebrium.com>
2023-01-18 11:55:46 +0200

    contract list: disclaimer

--- components/ContractsList.tsx
+++ components/ContractsList.tsx
@@ -10,6 +10,10 @@ interface IContractsListProps {
 export const ContractsList = (props: IContractsListProps) => (
   <BorderedPanel title="API3 Smart Contracts" big={true}>
     <div className="mt-0 mb-4 py-0">
+      <div className="disclaimer mb-2 text-center uppercase text-xs text-color-error">
+        Please do not send money to these contracts <br />
+        and just use the DAO UI to deposit/stake/vote
+      </div>
       {props.contracts.map((contract: IContract) => (
         <div key={contract.name} className="mb-2">
           <label className="text-color-cell-title text-xs font-bold">
