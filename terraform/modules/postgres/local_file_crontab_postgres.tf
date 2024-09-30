eb50479b8f8afb34b0a516ea8a819e1ff7f70e60
Aaron <blue@aquarat.co.uk>
2024-09-30 09:45:53 +0100

    Add indexes to improve performance

--- terraform/modules/postgres/local_file_crontab_postgres.tf
+++ terraform/modules/postgres/local_file_crontab_postgres.tf
@@ -1,6 +1,6 @@
 resource "local_file" "crontab_postgres" {
   content = <<EOF
-0 1 * * * root cd ${path.cwd} && bash ./bin/postgres-backup.sh >> /var/log/postgres-backups.log 2>&1
+24 4 * * */3 root cd ${path.cwd} && bash ./bin/postgres-backup.sh >> /var/log/postgres-backups.log 2>&1
 EOF
 
   filename = "./cron.d/crontab_postgres"
